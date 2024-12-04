592d872afb4fcc97019d995b2b66201d6899b723
Nicolay Nerdal <nicolay.nerdal@smn.no>
2024-12-04 07:22:12 +0000

    Video 2: MLP

--- mymore.py
+++ mymore.py
@@ -1,3 +1,4 @@
+import random
 import torch
 import torch.nn.functional as F
 
@@ -54,53 +55,102 @@ for i in range(5):
 # ----------------------------------------------
 # Neural network approach
 # ----------------------------------------------
-## Create training set of bigrams (x,y)
-xs, ys = [], []
-for w in words:
-    chs = ["."] + list(w) + ["."]
-    for ch1, ch2 in zip(chs, chs[1:]):
-        ix1 = stoi[ch1]
-        ix2 = stoi[ch2]
-        xs.append(ix1)
-        ys.append(ix2)
-xs = torch.tensor(xs)
-ys = torch.tensor(ys)
-num = xs.nelement()
+## Building the data set
 
-# Randomly initialize 27 neuron's weight. each neuron recieves 27 inputs
-W = torch.randn((27, 27), generator=g, requires_grad=True)
+block_size = (
+    3  # context length: how many characters do we take to predict the next one?
+)
 
-## gradient descent
-for k in range(10):
+
+def build_dataset(words):
+    X, Y = [], []
+    for w in words:
+        context = [0] * block_size
+        for ch in w + ".":
+            ix = stoi[ch]
+            X.append(context)
+            Y.append(ix)
+            # print(''.join(itos[i] for i in context), '--->', itos[ix])
+            context = context[1:] + [ix]  # crop and append
+
+    X = torch.tensor(X)
+    Y = torch.tensor(Y)
+    print(X.shape, Y.shape)
+    return X, Y
+
+
+random.seed(42)
+random.shuffle(words)
+n1 = int(0.8 * len(words))
+n2 = int(0.9 * len(words))
+
+Xtr, Ytr = build_dataset(words[:n1])
+Xdev, Ydev = build_dataset(words[n1:n2])
+Xte, Yte = build_dataset(words[n2:])
+
+
+# Create the lookup table (embeddings matrix)
+C = torch.randn((27, 10), generator=g)
+W1 = torch.randn((30, 200), generator=g)
+b1 = torch.randn(200, generator=g)
+W2 = torch.randn((200, 27), generator=g)
+b2 = torch.randn(27, generator=g)
+parameters = [C, W1, b1, W2, b2]
+
+
+for p in parameters:
+    p.requires_grad = True
+
+lri = []
+lossi = []
+stepi = []
+
+for i in range(100):
+    # minibatch construct
+    ix = torch.randint(0, Xtr.shape[0], (32,))
+
     # forward pass
-    xenc = F.one_hot(
-        xs, num_classes=27
-    ).float()  # input to the network: one-hot encoding
-    logits = xenc @ W  # predict log-counts
-    counts = logits.exp()  # counts equivalent to N
-    probs = counts / counts.sum(1, keepdim=True)  # probabilities for next character
-    loss = -probs[torch.arange(num), ys].log().mean()
-    print(loss.item())
+    emb = C[Xtr[ix]]  # (32, 3, 2)
+    h = torch.tanh(emb.view(-1, 30) @ W1 + b1)  # (32, 100)
+    logits = h @ W2 + b2  # (32, 27)
+    loss = F.cross_entropy(logits, Ytr[ix])
 
-    # Backward pass
-    W.grad = None  # set gradient to zero
+    # backward pass
+    for p in parameters:
+        p.grad = None
     loss.backward()
 
     # update
-    W.data += -0.1 * W.grad
+    lr = 0.1 if i < 10000 else 0.01
+    for p in parameters:
+        p.data += -lr * p.grad
+
+    stepi.append(i)
+    lossi.append(loss.log10().item())
 
 
-## inference
-for i in range(5):
+emb = C[Xdev]
+h = torch.tanh(emb.view(-1, 30) @ W1 + b1)
+logits = h @ W2 + b2
+loss = F.cross_entropy(logits, Ydev)
+loss
+
+
+# Inference
+for _ in range(20):
     out = []
-    ix = 0
+    context = [0] * block_size
     while True:
-        xenc = F.one_hot(torch.tensor([ix]), num_classes=27).float()
-        logits = xenc @ W
-        counts = logits.exp()
-        p = counts / counts.sum(1, keepdim=True)
-        ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
-        out.append(itos[ix])
+        emb = C[torch.tensor([context])]
+        h = torch.tanh(emb.view(1, -1) @ W1 + b1)
+        logits = h @ W2 + b2
+        probs = F.softmax(logits, dim=1)
+        ix = torch.multinomial(
+            probs, num_samples=1, replacement=True, generator=g
+        ).item()
+        context = context[1:] + [ix]
+        out.append(ix)
         if ix == 0:
             break
-    print("".join(out))
+
+    print("".join(itos[i] for i in out))
